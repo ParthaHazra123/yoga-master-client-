a184b6bef018515cad1a61298e37958547b5c605
Partha Hazra <hazrapartha2@gmail.com>
2026-01-12 23:23:37 +0530

    Add files via upload

---
+++ src/routes/Privet/PrivetRoute.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { useAuth } from '../../hooks/useAuth';
+import { Navigate, useLocation } from 'react-router-dom';
+
+const PrivetRoute = ({ children }) => {
+    const { user } = useAuth();
+    const location = useLocation();
+    if (!user) {
+        return <Navigate to="/login" state={{from : location.pathname}} />;
+    }
+    else {
+        return children;
+    }
+};
+
+export default PrivetRoute;
